207eb886d76ca09a730fec420f3761620b741db2
Visweswar1387 <gvisweswar0@gmail.com>
2019-04-02 19:12:17 +0530

    Adstcase for validating registration number

--- src/test/java/com/ParkingLot/Entity/CarTest.java
+++ src/test/java/com/ParkingLot/Entity/CarTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class CarTest {
+
     @Test
     public void should_Test_Create_Car_With_Registration_Number_And_Colour() {
         String regNo = "1234";
@@ -12,5 +13,12 @@ public class CarTest {
         Assert.assertEquals(car.getRegistrationNumber(), regNo);
         Assert.assertEquals(car.getColour(), colour);
 
+    }
+
+    @Test(expected = RegistartionNumberIsNotNullException.class)
+    public void should_Test_Registration_Number_Is_Not_Null() {
+        String regNo = "1234";
+        String colour = "Blue";
+        Car car = new Car(regNo, colour);
     }
 }
